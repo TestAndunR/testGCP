06bd72abdbfa495d9fe672ed31f8e80f094af13d
TestAndunR <37790187+TestAndunR@users.noreply.github.com>
2018-06-07 11:24:13 +0530

    Add remainin cloud storage opertions and api gateway

--- testGCP/lambda.js
+++ testGCP/lambda.js
@@ -3,6 +3,10 @@ let _auth = require('./Authorizer');
 let google = require('googleapis').google;
 const storage = google.storage('v1');
 exports.handler = function (event, context, callback) {
+
+	let encodedImage = event.image;
+	let content = Buffer.from(encodedImage, 'base64');
+
 	storage.objects.list({
 		bucket: 'staging.testgcpsigma.appspot.com',
 		maxResults: 10,
@@ -15,16 +19,58 @@ exports.handler = function (event, context, callback) {
 		.catch(err => {
 			console.log(err, err.stack); // an error occurred
 		});
-	storage.objects.insert({
-		"bucket": "staging.testgcpsigma.appspot.com",
-		"name": "image",
-		"media": {
-			"body": content
-		}
+	// storage.objects.insert({
+	// 	"bucket": "staging.testgcpsigma.appspot.com",
+	// 	"name": "image",
+	// 	"media": {
+	// 		"body": content
+	// 	}
+	// })
+	// 	.then(response => {
+	// 		console.log(response.data);           // successful response
+
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err, err.stack); // an error occurred
+	// 	});
+
+
+	// storage.objects.copy({
+	// 	sourceBucket: "staging.testgcpsigma.appspot.com",
+	// 	sourceObject: "image",
+	// 	destinationBucket: "testgcpsigma.appspot.com",
+	// 	destinationObject: "copy"
+	// })
+	// 	.then(response => {
+	// 		console.log(response.data);           // successful response
+
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err, err.stack); // an error occurred
+	// 	});
+
+	// storage.objects.delete({
+	// 	bucket: "testgcpsigma.appspot.com",
+	// 	object: "copy"
+	// })
+	// 	.then(response => {
+	// 		console.log(response.data);   
+	// 		console.log("Object deleted");        // successful response
+	// 		/*
+	// 			response.data = {}
+	// 		*/
+	// 	})
+	// 	.catch(err => {
+	// 		console.log(err, err.stack); // an error occurred
+	// 	});
+
+	storage.objects.get({
+		bucket: "staging.testgcpsigma.appspot.com",
+		object: "image"
 	})
-		.then(response => {
+		.then(data => {
 			console.log(response.data);           // successful response
-			
+
 		})
 		.catch(err => {
 			console.log(err, err.stack); // an error occurred
